62218d725190480eded3cb9c8084313a3990d5d0
dlgkdhq <dlgkdms920@naver.com>
2025-08-05 10:09:51 +0900

    메인페이지 작업 시작

---
+++ src/main/java/com/app/controller/mainpage/MainpageController.java
@@ -0,0 +1,13 @@
+package com.app.controller.mainpage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainpageController {
+	
+	@RequestMapping("/")
+	public String mainPage() {
+		return "mainpage/mainpage";
+	}
+}
